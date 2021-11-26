bbcd17d11b96c37e3f1a90765742270df10e52aa
AliceMenzie <alice.menzie@gmail.com>
2021-11-26 11:04:02 +1100

    add hero section with card component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import explore from '../assets/explore.jpg'
 
 export default function Main() {
     return (
-        <div>
-            MAIN HERE 
+        <div className="main-container">
+            <div className="main-img-container">
+            {/* <img className="img-explore" src={explore} alt="explore" /> */}
+            </div>
+            
+            <div className="main-card">
+                <h2>Welcome back!</h2>
+                <p>Open daily 10am-5pm</p>
+                <p>We're delighted that Melbourne Museum has reopened. Don't miss out pre-book your tickets today!</p>
+                <p>What's on</p>
+                <p>Get tickets</p>
+                <p>Visiting Information</p>
+            </div>
         </div>
     )
 }
